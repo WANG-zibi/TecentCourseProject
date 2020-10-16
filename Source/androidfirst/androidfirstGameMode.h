23db822ebc18b52b805638e40a357b9aee357f34
DESKTOP-ONV4AEA\Administrator <wtyxxs@foxmail.com>
2020-10-16 13:10:36 +0800

    Initial commit

---
+++ Source/androidfirst/androidfirstGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "androidfirstGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AandroidfirstGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AandroidfirstGameMode();
+};
+
+
+
