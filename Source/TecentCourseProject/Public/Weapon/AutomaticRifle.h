6afb0de7fc7802ec076844d49728a2471b33511a
kingskyuniverse <wtyxxs@foxmail.com>
2020-12-16 21:41:03 +0800

    优化榴弹枪

---
+++ Source/TecentCourseProject/Public/Weapon/AutomaticRifle.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/TCPWeapon.h"
+#include "AutomaticRifle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TECENTCOURSEPROJECT_API AAutomaticRifle : public ATCPWeapon
+{
+	GENERATED_BODY()
+	
+};
